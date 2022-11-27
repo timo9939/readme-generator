d6953f4ab0068edd178748d7f73ff2fa13b05faf
timo9939 <timothylau20199@gmail.com>
2022-11-27 13:05:08 +0000

    question added

--- potential-enigma-main/Develop/index.js
+++ potential-enigma-main/Develop/index.js
@@ -1,14 +1,71 @@
 // TODO: Include packages needed for this application
-const inquirer=require(inquirer);
+const inquirer=require('inquirer');
 const fs =require('fs')
 // TODO: Create an array of questions for user input
-const questions = [];
+//const questions = [];
 
-// TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+//const generateHTML=({title})
+
+inquirer
+.prompt([
+    {
+        type:"input",
+        name:"title",
+        message: 'What is the title of the project ?'
+    },
+    {
+     type:'input',
+     name:'description',
+     message:'Input description'   
+    },
+    {
+        type:'input',
+        name:'installation',
+        message:'Input the installation instruction'
+    },
+    {
+        type:'input',
+        name:'usage-information',
+        message:'Input the usage information'
+    },
+    {
+        type:'input',
+        name:'contribution',
+        message:'Input contribuation'
+    },
+    {
+        type:'input',
+        name:'Test',
+        message:'Input test'
+    },
+    {
+        type:'list',
+        name:'license',
+        message:'Input the license',
+        choices:["None","Apache license 2.0","Artistic license 2.0"]
+    },
+    {
+        type:'input',
+        name:'gitHub-username',
+        message:'Input your GitHub username'
+    },
+    {
+        type:'input',
+        name:'email',
+        message:'Input your email address'
+    }
+])
+// .then ((reaction)=>{
+//     const readmeContent= generateHTML(reaction);
+// })
+// // TODO: Create a function to write README file
+// fs.writeFile(readme.md, readmeContent, (err) =>
+// err ? console.log(err) : console.log('Successfully created README file!')
+// );
+
 
 // TODO: Create a function to initialize app
-function init() {}
+// function init() {}
 
 // Function call to initialize app
-init();
+// init();
